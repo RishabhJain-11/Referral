aec7c70244e7686468d5964733a821155010b98e
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-05-31 12:30:53 +0530

    31-05-2023

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,10 +5,24 @@ router.get('/', (req, res) => {
     res.json({ message: 'Get all workouts' })
 })
 
+
 router.get('/:id', (req, res) => {
     res.json({ message: 'Get single workout' })
 })
 
-router.post('/', )
+
+router.post('/', (req, res) => {
+    res.json({ message: "Post a new workout" })
+})
+
+
+router.delete('/:id', (req, res) => {
+    res.json({ message: "Delete a new workout" })
+})
+
+
+router.patch('/:id', (req, res) => {
+    res.json({ message: "Update a new workout" })
+})
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const workoutRoutes = require('./routes/workouts');
+const mongoose = require('mongoose');
+
+//middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
-    next()
+    next();
 })
 
 app.use('/api/workouts', workoutRoutes);
 
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, (e) => {
+            if (!e) {
+                console.log(`Server listening on localhost:${port}`);
+            } else {
+                console.log(e.message);
+            }
+        })
 
-app.listen(port, (e) => {
-    if (!e) {
-        console.log(`Server listening on localhost:${port}`);
-    } else {
+    })
+    .catch((e) => {
         console.log(e.message);
-    }
-})
+    })
+
