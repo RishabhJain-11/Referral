d1d6d1485ae68ffee6bf255cd1e5cb4cd9e465e9
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-06-20 21:16:55 +0530

    20-06-2023

--- frontend/src/pages/Footer.js
+++ frontend/src/pages/Footer.js
@@ -3,12 +3,6 @@ import React from 'react'
 export default function Footer() {
     return (
         <section>
-            {/* <footer>
-                <div class="text">
-                    <span>Created By <a href="www.github.com/RishabhJain-11">Rishabh Jain Company</a> | &#169; 2023 All Rights Reserved</span>
-                </div>
-            </footer> */}
-
             <footer class="bg-white dark:bg-gray-900">
                 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div class="md:flex md:justify-between">
